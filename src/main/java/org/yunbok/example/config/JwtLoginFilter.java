f8494f10b4f81b7110b2f23bed5a3da8cf0564ca
Yunbok <qodbsqhr@naver.com>
2024-04-02 23:29:10 +0900

    remove unused comments

--- src/main/java/org/yunbok/example/config/JwtLoginFilter.java
+++ src/main/java/org/yunbok/example/config/JwtLoginFilter.java
@@ -45,22 +45,7 @@ public class JwtLoginFilter extends UsernamePasswordAuthenticationFilter {
     //로그인 성공시 실행하는 메소드 (여기서 JWT를 발급하면 됨)
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
-
         System.out.println("success");
-
-//        User customUserDetails = (User) authentication.getPrincipal();
-//
-//        String username = customUserDetails.getUsername();
-//
-//        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-//        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
-//        GrantedAuthority auth = iterator.next();
-//
-//        String role = auth.getAuthority();
-//
-//        String token = jwtUtil.createJwt(username, role, 60*60*10L);
-//
-//        response.addHeader("Authorization", "Bearer " + token);
     }
 
     @Override
